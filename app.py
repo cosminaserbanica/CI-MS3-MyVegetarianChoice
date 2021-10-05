01d93d3ae5c9957307a1818fd702c14784ec73bd
cosminaserbanica <cosmina.serbanica@yahoo.com>
2021-10-05 12:10:06 +0100

    Add spaces for PEP8 compliance

--- app.py
+++ app.py
@@ -7,16 +7,25 @@ from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
+
+
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
+
 mongo = PyMongo(app)
+
+
 @app.route("/")
+
 @app.route("/get_tasks")
 def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
