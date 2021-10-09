a58fc856cead04cfa936011c3a8b8414f32b58f4
cosminaserbanica <cosmina.serbanica@yahoo.com>
2021-10-09 14:49:34 +0100

    Add autoplay function for carousel

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
   $('.sidenav').sidenav({edge: "right"});
-  $('.carousel').carousel();
+  //https://stackoverflow.com/questions/36581504/materialize-carousel-slider-autoplay 
+  $('.carousel').carousel({
+    padding: 200    
+});
+autoplay();
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 2000);
+}
 });
