e649f63bfd471ec1091adaff44103fb128a32904
cosminaserbanica <cosmina.serbanica@yahoo.com>
2021-10-28 22:30:36 +0100

    Add pagination functionality

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from flask import Blueprint
-from flask_paginate import Pagination, get_page_parameter
+from flask_paginate import Pagination, get_page_parameter, get_page_args
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
@@ -19,12 +19,58 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+mod = Blueprint('users', __name__)
 
+
+PER_PAGE = 3
+
+
+def paginated(recipes):
+
+    # Set pagination configuration
+    page, per_page, offset = get_page_args(page_parameter="page",
+                                          per_page_parameter="per_page")
+
+    offset = page * PER_PAGE - PER_PAGE
+
+    return recipes[offset: offset + PER_PAGE]
+
+
+def pagination_args(recipes):
+
+    # Set pagination configuration
+    page, per_page, offset = get_page_args(page_parameter="page",
+                                          per_page_parameter="per_page")
+
+    total = len(recipes)
+
+    return Pagination(page=page,
+                      per_page=PER_PAGE,
+                      total=total,
+                      css_framework="bootstrap5")
+
+
+@mod.route("/")
 @app.route("/")
 
 def get_recipes():
     recipes = mongo.db.recipes.find()
-    return render_template("recipes.html", recipes=recipes)
+    search = False
+    q = request.args.get('q')
+    if q:
+        search = True
+
+    page = request.args.get(get_page_parameter(), type=int, default=1)
+
+    pagination = Pagination(page=page, total=recipes.count(), search=search, record_name='recipes', show_single_page=True)
+    # 'page' is the default name of the page parameter, it can be customized
+    # e.g. Pagination(page_parameter='p', ...)
+    # or set PAGE_PARAMETER in config file
+    # also likes page_parameter, you can customize for per_page_parameter
+    # you can set PER_PAGE_PARAMETER in config file
+    # e.g. Pagination(per_page_parameter='pp')
+
+    return render_template('recipes.html', per_page=3, recipes=recipes, pagination=pagination)
 
 
 @app.route("/home")
